f3f80d4b67f1e491be97cd45074bc530e4f3e780
Isabela <isabela_cmoreira@hotmail.com>
2022-06-14 14:12:25 -0300

    feat: added posts, likes, tags and sessions tables

--- src/config/tables.sql
+++ src/config/tables.sql
@@ -6,3 +6,29 @@ CREATE TABLE IF NOT EXISTS "users"(
   "pictureUrl" text NOT NULL,
   "createdAt" timestamp NOT NULL DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS "posts"(
+  "id" SERIAL PRIMARY KEY,
+  "userId" INTEGER REFERENCES users(id),
+  "link" TEXT NOT NULL UNIQUE,
+  "description" VARCHAR(255) NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL DEFAULT NOW ()
+);
+
+CREATE TABLE IF NOT EXISTS "likes"(
+  "id" SERIAL PRIMARY KEY,
+  "userId" INTEGER REFERENCES users(id), 
+  "postId" INTEGER REFERENCES posts(id), 
+)
+
+CREATE TABLE IF NOT EXISTS "tags"(
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "sessions" (
+  "id" SERIAL PRIMARY KEY,
+  "token" TEXT NOT NULL UNIQUE,
+  "userId" INTEGER NOT NULL REFERENCES users(id),
+  "createdAt" TIMESTAMP NOT NULL DEFAULT NOW ()
+);
