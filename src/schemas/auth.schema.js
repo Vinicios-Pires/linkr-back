f08e1123b9d2efca9cd1de7e4668203dc6468b9c
Celso <celsobenedetti2@gmail.com>
2022-06-14 12:10:35 -0300

    feat: signIn/signUp Joi schemas

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -1,3 +1,13 @@
-/*
-const SignInSchema = Joi.object(...)
-*/
+import Joi from "joi";
+
+export const SignUpSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  pictureUrl: Joi.string().uri().required(),
+});
+
+export const SignInSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
