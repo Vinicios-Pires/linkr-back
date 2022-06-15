75138fede18b29cab1e2a57bc4b7c009454ebc55
Vinicios Pires <contato.viniciospires@gmail.com>
2022-06-15 04:53:05 -0300

    fix: postRouter

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express, { json } from "express";
 import cors from "cors";
 import router from "./routers/index.js";
 
-import postRouter from "./routers/posts.router.js";
-
 const app = express();
 
 app.use(cors());
