bcf4fd7936a5f538d6e7b06cf8383bf778bb94f3
Isabela <isabela_cmoreira@hotmail.com>
2022-06-20 07:05:35 -0300

    feat: function created to get posts by id

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -11,9 +11,14 @@ const getPosts = async () => {
   return db.query(`SELECT "postURL", description FROM posts ORDER BY timestamp DESC LIMIT 20;`);
 };
 
+const getPostsById = async (id)=> {
+  return db.query (`SELECT * FROM posts WHERE id = $1`,[id]);
+}
+
 const postsRepository = {
   createPost,
   getPosts,
+  getPostsById
 };
 
 export default postsRepository;
