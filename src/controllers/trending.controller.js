065d104692d3327f7d1f10130a5728433d40f0c1
Pimenta <mateuspimartins@hotmail.com>
2022-06-24 12:29:49 -0300

    trending controller

---
+++ src/controllers/trending.controller.js
@@ -0,0 +1,53 @@
+import { trendingRepository } from "../repositories/trending.repository.js"
+
+async function getTrendings(req, res) {
+  const LIMIT_TRENDINGS = 10
+
+  try {
+    const { rows: trendings } = await trendingRepository.getTrendingHashtags(
+      LIMIT_TRENDINGS,
+    )
+
+    res.send(trendings)
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+}
+
+const getHashtagPosts = async (req, res) => {
+  const { hashtag } = req.params;
+  console.log(hashtag)
+  try {
+    const postsUser = await trendingRepository.getPostsByHash(hashtag);
+    console.log(postsUser)
+    res.status(200).send(postsUser);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+};
+
+const trendingController = {
+  getTrendings,
+  getHashtagPosts,
+}
+
+export default trendingController;
+
+// export const getHashtagPosts = async (req, res) => {
+//   const { hashtag } = req.params;
+//   try {
+//     const { rows: posts } = await trendingRepository.getPostsByHash(hashtag);
+//       res.status(200).send(posts);
+//     if (posts.rowCount > 0) {
+//       return res.status(409).send("There are no posts yet");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     return res
+//       .status(500)
+//       .send("An error occured while trying to fetch the posts, please refresh the page"); // server error
+//   }
+// };
+
+
